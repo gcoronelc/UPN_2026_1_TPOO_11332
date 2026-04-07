c81e72573104d0ad39aef379b62035664d38b574
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2026-04-07 06:40:45 -0500

    Semana 3

---
+++ Semana03_06ABR26/Semana0302/Semana0302/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Semana0302
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnCrear = new Button();
+            btnMostrar = new Button();
+            dgvNombres = new DataGridView();
+            label1 = new Label();
+            txtNombre = new TextBox();
+            nombre = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dgvNombres).BeginInit();
+            SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            btnCrear.Location = new Point(32, 103);
+            btnCrear.Name = "btnCrear";
+            btnCrear.Size = new Size(75, 23);
+            btnCrear.TabIndex = 0;
+            btnCrear.Text = "Crear";
+            btnCrear.UseVisualStyleBackColor = true;
+            btnCrear.Click += btnCrear_Click;
+            // 
+            // btnMostrar
+            // 
+            btnMostrar.Location = new Point(197, 103);
+            btnMostrar.Name = "btnMostrar";
+            btnMostrar.Size = new Size(75, 23);
+            btnMostrar.TabIndex = 1;
+            btnMostrar.Text = "Mostrar";
+            btnMostrar.UseVisualStyleBackColor = true;
+            btnMostrar.Click += btnMostrar_Click;
+            // 
+            // dgvNombres
+            // 
+            dgvNombres.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvNombres.Columns.AddRange(new DataGridViewColumn[] { nombre });
+            dgvNombres.Location = new Point(32, 132);
+            dgvNombres.Name = "dgvNombres";
+            dgvNombres.Size = new Size(240, 150);
+            dgvNombres.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(99, 26);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(173, 23);
+            txtNombre.TabIndex = 4;
+            // 
+            // nombre
+            // 
+            nombre.HeaderText = "Nombre";
+            nombre.Name = "nombre";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(307, 330);
+            Controls.Add(txtNombre);
+            Controls.Add(label1);
+            Controls.Add(dgvNombres);
+            Controls.Add(btnMostrar);
+            Controls.Add(btnCrear);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgvNombres).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnCrear;
+        private Button btnMostrar;
+        private DataGridView dgvNombres;
+        private Label label1;
+        private TextBox txtNombre;
+        private DataGridViewTextBoxColumn nombre;
+    }
+}
