28ad38f6edb851dc65d2ec3bb49cb149074d0781
Gustavo Coronel <gcoronelc@gmail.com>
2026-05-12 10:38:14 -0500

    Semana 8

--- Semana08_11MAY26/EducaApp/src/main/java/pe/edu/upn/educaapp/EducaApp.java
+++ Semana08_11MAY26/EducaApp/src/main/java/pe/edu/upn/educaapp/EducaApp.java
@@ -1,8 +1,11 @@
 package pe.edu.upn.educaapp;
 
+import pe.edu.upn.educaapp.view.LogonView;
+
 public class EducaApp {
 
     public static void main(String[] args) {
         System.out.println("Hello Gustavo!!!");
+        LogonView.main(args);
     }
 }
